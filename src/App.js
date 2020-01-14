367526e133c1d39b0ca8fd21d0de2f3a22fa36dc
woeph7 <aeishin0212@gmail.com>
2020-01-14 21:41:37 +0100

    menu animate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring'
 import Toggle from './Toggle'
+import Nav from './Nav'
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = React.useState(false)
+  const navAnimate = useSpring({ transform: isNavOpen ? `translate3d(0,0,0)` : `translate3d(100%,0,0)` })
+
   const fade = useSpring({
     from: {
       opacity: 0
@@ -15,10 +19,12 @@ const App = () => {
   })
 
   return (
+
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt="logo" />
-        <button className="menu-button">Menu</button>
+        <button className="menu-button" onClick={() => setNavOpen(!isNavOpen)}>Menu</button>
+        <Nav style={navAnimate} />
       </header>
       <main>
         <Toggle />
