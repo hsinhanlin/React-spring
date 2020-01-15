108294a5c538b371893e7dfac60b5ac478b1297a
woeph7 <aeishin0212@gmail.com>
2020-01-15 09:44:14 +0100

    keyframe

--- src/Toggle.js
+++ src/Toggle.js
@@ -5,15 +5,19 @@ const Toggle = () => {
     const [isToggled, setToggled] = React.useState(false)
     const { color, x } = useSpring({
         color: isToggled ? 'tomato' : 'lightgreen',
-        x: isToggled ? 0 : 50
+        x: isToggled ? 0 : 1
     })
 
     return (
         <div>
-            <animated.h1 style={{
-                transform: x.interpolate(x => `translate3d(${x}px,0,0)`),
-                color
-            }} >hello</animated.h1>
+            <animated.h1
+                style={{
+                    transform: x.interpolate({
+                        range: [0, .25, .50, .75, 1],
+                        output: [0, -75, 10, 0, 0]
+                    }).interpolate(x => `translate3d(${x}px,0,0)`),
+                    color
+                }} >hello</animated.h1>
             <button onClick={() => setToggled(!isToggled)}>toggle</button>
         </div>
     )
