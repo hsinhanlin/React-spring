1624c112161153f0abb6da91a0b1c8f42aaa95ab
woeph7 <aeishin0212@gmail.com>
2020-01-15 10:59:18 +0100

    transition group

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
+import React from "react";
+import { useTransition, animated } from "react-spring";
 
 const Toggle = () => {
-    const [isToggled, setToggled] = React.useState(false)
-    const { color, x } = useSpring({
-        color: isToggled ? 'tomato' : 'lightgreen',
-        x: isToggled ? 0 : 1
-    })
+    const [items, setItems] = React.useState([{
+        letter: "s",
+        key: 1
+    }, {
+        letter: "o",
+        key: 2
+    }, {
+        letter: "s",
+        key: 3
+    }, {
+        letter: "o",
+        key: 4
+    }]);
+    const transition = useTransition(items, item => item.key, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
+    });
 
     return (
         <div>
-            <animated.h1
-                style={{
-                    transform: x.interpolate({
-                        range: [0, .25, .50, .75, 1],
-                        output: [0, -75, 10, 0, 0]
-                    }).interpolate(x => `translate3d(${x}px,0,0)`),
-                    color
-                }} >hello</animated.h1>
-            <button onClick={() => setToggled(!isToggled)}>toggle</button>
+            {transition.map(({ item, key, props }) =>
+                <animated.h1 style={props} key={key}>{item.letter}</animated.h1>
+            )}
+            <button onClick={() => setItems([{
+                letter: "s",
+                key: 1
+            }])}>toggle</button>
         </div>
-    )
-}
+    );
+};
 
-
-export default Toggle
+export default Toggle;
