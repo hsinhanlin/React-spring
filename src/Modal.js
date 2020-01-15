c01f61157d3a99b9455077dfdc280492dd0c7580
woeph7 <aeishin0212@gmail.com>
2020-01-15 13:16:17 +0100

    modal bg ease

--- src/Modal.js
+++ src/Modal.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { useTransition, animated } from 'react-spring'
 
 
-const Modal = ({ closeModal, modalAnimate }) => {
+const Modal = ({ closeModal, modalAnimate, pointerEvents }) => {
     return (
-        <animated.div className="modal" style={modalAnimate}>
-            <div className="modal-card">
+        <div className="modal" style={{ pointerEvents }, { background: 'rgba(0, 0, 0, .75)' }}>
+            <animated.div className="modal-card" style={modalAnimate}>
                 <h1>Modal</h1>
                 <button onClick={closeModal}>close</button>
-            </div>
-        </animated.div>
+            </animated.div>
+        </div>
     )
 }
 
@@ -18,14 +18,22 @@ const ModalWrapper = () => {
     const transition = useTransition(on, null, {
         from: { opacity: 0, transform: 'translate3d(0,-40px,0)' },
         enter: { opacity: 1, transform: 'translate3d(0,0,0)' },
-        leave: { opacity: 0, transform: 'translate3d(0,-40px,0)' },
+        leave: { opacity: 0, transform: 'translate3d(0,-40px,0)' }
     })
+
+    const pointerEvents = on ? 'all' : 'none'
+
     return (
         <div>
             {transition.map(
                 ({ item, key, props: modalAnimate }) =>
                     item && (
-                        <Modal modalAnimate={modalAnimate} closeModal={() => toggle(false)} key={key} />
+                        <Modal
+                            modalAnimate={modalAnimate}
+                            closeModal={() => toggle(false)}
+                            key={key}
+                            pointerEvents={pointerEvents}
+                        />
                     )
             )}
             <button onClick={() => toggle(!on)}>Open</button>
